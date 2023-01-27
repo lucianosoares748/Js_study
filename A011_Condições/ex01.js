890611052921e07cba76d3512f1f1e2ae8d30121
luciano soares <lucianosoaresdev748@gmail.com>
2023-01-27 16:02:20 -0300

    first commit

---
+++ A011_Condições/ex01.js
@@ -0,0 +1,9 @@
+var vel = 100
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+
+//abaixo descrevi uma condição simples possuindo apenas o TRUE
+if (vel >= 80) {
+  console.log('Você ultrapassou o limite de velocidade e foi MULTADO!')
+}
+
+console.log('Dirija com cuidado, sempre use o cinto de segurança!')
---
+++ A011_Condições/ex02.js
@@ -0,0 +1,8 @@
+var pais = "EUA";
+
+//CONDIÇÃO COMPOSTA = onde existem as duas possibilidades de if e else
+if (pais == "brasil") {
+  console.log("voce é Brasileiro!");
+} else {
+  console.log("voce é Estrangeiro!");
+}
