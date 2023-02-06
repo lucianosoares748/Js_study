f7ace0794a03699d80b724f13f88b62e7b51b48c
luciano soares <lucianosoaresdev748@gmail.com>
2023-02-06 19:22:55 -0300

    study repetição

---
+++ A13_Repetição/ex01.js
@@ -0,0 +1,18 @@
+/* 
+Repetição 'while' aqui ele primeiro testa a condição e depois executa o bloco 
+*/
+var c = 1;
+
+while (c <= 6) {
+  console.log(`Passo ${c}`);
+  c++;
+}
+
+/*
+Agora iremos ver o 'do while' que irá executar primeiro o bloco e depois testar a condição
+*/
+var n = 1;
+do {
+  console.log(`Passo ${n}`);
+  n++;
+} while (n <= 6);
