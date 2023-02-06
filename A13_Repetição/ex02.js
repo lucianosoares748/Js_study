2617b2c82e9f5798c8d75dbf10168e4d2519c0bd
luciano soares <lucianosoaresdev748@gmail.com>
2023-02-06 19:46:42 -0300

    finis repet

---
+++ A13_Repetição/ex02.js
@@ -0,0 +1,17 @@
+/*
+Estrutura de repetição com variável de controle 
+*/
+for (var n = 1; n <= 6; n++) {
+  console.log(`Repetição ${n}`);
+}
+
+//------------------------------------------------------
+/*AQUI TEMOS O MESMO EXEMPLO PORÉM SEM A APENAS COM O WHILE
+var c = 1;
+
+while (c <= 6) {
+  console.log(`Passo ${c}`);
+  c++;
+}
+
+*/
