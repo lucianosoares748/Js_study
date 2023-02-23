c59ac0a7db88e3fbd1765760f964b9154496ddf0
luciano soares <lucianosoaresdev748@gmail.com>
2023-02-23 18:08:41 -0300

    end study parameter function

---
+++ A16_Functions/ex02.js
@@ -0,0 +1,12 @@
+function soma(n1 = 0, n2 = 0) {
+  return n1 + n2;
+}
+
+//aqui eu estou fazendo a chamada de dentro do console.log passando também os parâmetros
+console.log(soma(2, 542));
+
+/*
+obs: Se na função eu estiver esperando dois parâmetros e a função só receber 1 parâmetro 
+vai dar ERRO a não ser que eu pré defina os parâmetros dentro da função como nesse exemplo
+que diz se n1 ou n2 não receber nada eles serão iguais a 0 
+*/
